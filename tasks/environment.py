3047f2290a530e7548f208d5bb8525b884ac6550
Dilip <dilip.arumugam@gmail.com>
2019-11-21 21:22:21 -0800

    Resolving config dim bug

--- tasks/environment.py
+++ tasks/environment.py
@@ -204,7 +204,9 @@ class Environment:
 
             # config = self.get_current_config()
             preconfig = parent.config
-            config = self.dynamics.forward(opt_cuda(torch.tensor(preconfig).float()).unsqueeze(0), opt_cuda(action).float())
+            with torch.no_grad():
+                config = self.dynamics.forward(opt_cuda(torch.tensor(preconfig).float()).unsqueeze(0), opt_cuda(action).float())
+            config = config[0].detach().cpu().numpy()
             action = action[0].detach().cpu().numpy()
 
             # count = 0

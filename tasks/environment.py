7fdb16f72d518ef11ff056adb8bc9c7984f6bcc9
Dilip <dilip.arumugam@gmail.com>
2019-11-22 14:52:40 -0800

    Adhering to gym interface

--- tasks/environment.py
+++ tasks/environment.py
@@ -290,8 +290,8 @@ class Environment:
             done = True
 
         # return opt_cuda(torch.unsqueeze(torch.tensor(self.get_current_config()), 0)), opt_cuda(torch.unsqueeze(torch.tensor(reward), 0)), [done], [{"episode": {"r": reward}}]
-        return opt_cuda(torch.unsqueeze(torch.tensor(self.get_current_config()), 0)), \
-               opt_cuda(torch.unsqueeze(torch.tensor(reward), 0)), opt_cuda(torch.tensor([done])), \
+        return opt_cuda(torch.tensor(self.get_current_config().unsqueeze(0).float())), \
+               opt_cuda(torch.tensor(reward).unsqueeze(0).float()), opt_cuda(torch.tensor([done])), \
                [{"episode": {"r": reward}}]
 
     def reset(self):
